2442fc0b634bd6773b33110edcce6f25bbe1c8dd
Fabrício Ronchi <fabricioronchii@gmail.com>
2014-10-02 13:55:42 -0300

    Login

--- app/routes.js
+++ app/routes.js
@@ -1,74 +1,46 @@
 module.exports = function(app, passport) {
 
-// normal routes ===============================================================
-
-	// show the home page (will also have our login links)
 	app.get('/', function(req, res) {
 		res.render('index.ejs');
 	});
 
-	// PROFILE SECTION =========================
 	app.get('/profile', isLoggedIn, function(req, res) {
 		res.render('profile.ejs', {
 			user : req.user
 		});
 	});
 
-	// LOGOUT ==============================
 	app.get('/logout', function(req, res) {
 		req.logout();
 		res.redirect('/');
 	});
 
-// =============================================================================
-// AUTHENTICATE (FIRST LOGIN) ==================================================
-// =============================================================================
+	app.get('/login', function(req, res) {
+		res.render('login.ejs', { message: req.flash('loginMessage') });
+	});
 
-	// locally --------------------------------
-		// LOGIN ===============================
-		// show the login form
-		app.get('/login', function(req, res) {
-			res.render('login.ejs', { message: req.flash('loginMessage') });
-		});
+	app.post('/login', passport.authenticate('local-login', {
+		successRedirect : '/profile',
+		failureRedirect : '/login',
+		failureFlash : true
+	}));
 
-		// process the login form
-		app.post('/login', passport.authenticate('local-login', {
-			successRedirect : '/profile', // redirect to the secure profile section
-			failureRedirect : '/login', // redirect back to the signup page if there is an error
-			failureFlash : true // allow flash messages
-		}));
+	app.get('/signup', function(req, res) {
+		res.render('signup.ejs', { message: req.flash('signupMessage') });
+	});
 
-		// SIGNUP =================================
-		// show the signup form
-		app.get('/signup', function(req, res) {
-			res.render('signup.ejs', { message: req.flash('signupMessage') });
-		});
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/profile',
+		failureRedirect : '/signup',
+		failureFlash : true
+	}));
 
-		// process the signup form
-		app.post('/signup', passport.authenticate('local-signup', {
-			successRedirect : '/profile', // redirect to the secure profile section
-			failureRedirect : '/signup', // redirect back to the signup page if there is an error
-			failureFlash : true // allow flash messages
-		}));
+	app.post('/connect/local', passport.authenticate('local-signup', {
+		successRedirect : '/profile',
+		failureRedirect : '/connect/local',
+		failureFlash : true
+	}));
 
-		app.get('/connect/local', function(req, res) {
-			res.render('connect-local.ejs', { message: req.flash('loginMessage') });
-		});
-		app.post('/connect/local', passport.authenticate('local-signup', {
-			successRedirect : '/profile', // redirect to the secure profile section
-			failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
-			failureFlash : true // allow flash messages
-		}));
-
-
-// =============================================================================
-// UNLINK ACCOUNTS =============================================================
-// =============================================================================
-// used to unlink accounts. for social accounts, just remove the token
-// for local account, remove email and password
-// user account will stay active in case they want to reconnect in the future
-
-	// local -----------------------------------
 	app.get('/unlink/local', isLoggedIn, function(req, res) {
 		var user            = req.user;
 		user.local.email    = undefined;
@@ -78,10 +50,10 @@ module.exports = function(app, passport) {
 		});
 	});
 
-// route middleware to ensure user is logged in
+};
+
 function isLoggedIn(req, res, next) {
 	if (req.isAuthenticated())
 		return next();
-
 	res.redirect('/');
 }
